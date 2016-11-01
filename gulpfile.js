25a01f501df7babfcfefdbfd87fe1679ab3bfd5f
jirishus <jirishus@yahoo.com>
2016-10-31 21:01:58 -0700

    gulp compass compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var compass = require('gulp-compass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
@@ -13,7 +14,18 @@ gulp.task('browser-sync', function() {
   });
 });
 
+gulp.task('styles', function() {
+  return gulp.src('sass/**/*.scss')
+  .pipe(compass({
+    css: './stylesheets',
+    sass: './sass'
+  }))
+  .pipe(gulp.dest('stylesheets'))
+});
+
 gulp.task('default', ['localserver', 'browser-sync'], function() {
   console.log('default job');
+  gulp.watch('sass/**/*.scss', ['styles']);
+  gulp.watch('stylesheets/*.css', browserSync.reload);
   gulp.watch('index.html', browserSync.reload);
 });
