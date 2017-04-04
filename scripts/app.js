d283d3e547c7ed1329df3b0a456909969f07f90c
jirishus <jirishus@yahoo.com>
2017-04-03 22:53:58 -0700

    basic form handling

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,32 @@
 (function() {
-  console.log('app init');
+  
+  var leadForm = document.getElementById('imc-form');
+  var loaderIcon = document.getElementById('loader-icon');
+  var downloadText = document.getElementById('download-text');
+
+  loaderIcon.style.visibility = "hidden";
+  downloadText.style.visibility = "hidden";
+
+  leadForm.addEventListener('submit', handleFormSubmit);
+
+  function handleFormSubmit(evt) {
+   evt.preventDefault();
+
+   var firstName = document.getElementById('firstName').value;
+   var lastName = document.getElementById('lastName').value;
+   var leadEmail = document.getElementById('leadEmail').value;
+
+   console.log(firstName);
+   console.log(lastName);
+   console.log(leadEmail);
+
+   loaderIcon.style.visibility = "visible";
+
+   setTimeout(function() {
+     leadForm.style.visibility = "hidden";
+     downloadText.style.visibility = "visible";
+   }, 1500);
+
+  };
+
 })();
