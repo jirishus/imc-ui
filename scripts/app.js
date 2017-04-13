b5eadb177e6e6458f2f5ea4da7ecc0dc0a9d05f5
jirishus <jirishus@yahoo.com>
2017-04-12 20:42:44 -0700

    updated layout and copy closer to intent

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,24 @@
 (function() {
   
-  var leadForm = document.getElementById('imc-form');
-  var loaderIcon = document.getElementById('loader-icon');
-  var downloadText = document.getElementById('download-text');
+  // var leadForm = document.getElementById('imc-form');
+  // var downloadText = document.getElementById('download-text');
 
-  loaderIcon.style.visibility = "hidden";
-  downloadText.style.visibility = "hidden";
+  // downloadText.style.visibility = "hidden";
 
-  leadForm.addEventListener('submit', handleFormSubmit);
+  // leadForm.addEventListener('submit', handleFormSubmit);
 
-  function handleFormSubmit(evt) {
-   evt.preventDefault();
+  // function handleFormSubmit(evt) {
+  //  evt.preventDefault();
 
-   var firstName = document.getElementById('firstName').value;
-   var lastName = document.getElementById('lastName').value;
-   var leadEmail = document.getElementById('leadEmail').value;
+  //  var firstName = document.getElementById('firstName').value;
+  //  var lastName = document.getElementById('lastName').value;
+  //  var leadEmail = document.getElementById('leadEmail').value;
 
-   console.log(firstName);
-   console.log(lastName);
-   console.log(leadEmail);
+  //  setTimeout(function() {
+  //    leadForm.style.visibility = "hidden";
+  //    downloadText.style.visibility = "visible";
+  //  }, 1500);
 
-   loaderIcon.style.visibility = "visible";
-
-   setTimeout(function() {
-     leadForm.style.visibility = "hidden";
-     downloadText.style.visibility = "visible";
-   }, 1500);
-
-  };
+  // };
 
 })();
