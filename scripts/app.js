83b9fc9ca653aa480fd01dc3ce27a78895cc53e4
jirishus <jirishus@yahoo.com>
2017-04-19 22:41:48 -0700

    Minor Email Input Update

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,23 @@
 (function() {
   
-
   var leadForm = document.getElementById('leadForm');
   var downloadBtn = document.getElementById('download-btn');
+  
   downloadBtn.style.visibility = 'hidden';
  
   leadForm.addEventListener('submit', function(evt) {
+    var emailInput = document.getElementById('emailInput').value;
+
     evt.preventDefault();
     
     // Show Download Button
-    leadForm.style.display = 'none';
-    downloadBtn.style.visibility = 'visible';
+    // leadForm.style.display = 'none';
+    // downloadBtn.style.visibility = 'visible';
 
-    console.log('form submit');
+    var emailRegex = /.com/i;
 
+    // console.log(emailRegex.test(emailInput));
+    
   });
 
   downloadBtn.addEventListener('click', function(evt) {
